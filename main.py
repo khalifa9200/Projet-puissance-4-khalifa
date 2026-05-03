ed3e74f286ab04251b03e53275f4e7c62ad6fa67
khalifa9200 <k.gherghout95@gmail.com>
2026-05-03 15:05:26 +0200

    khalifa

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import tkinter as tk
+from tkinter import messagebox
 
-def fen_accueil():
-    #Fenêtre de la liste
-    fenetre = tk.Toplevel()
-    fenetre.title("Puissance 4")
-    
-    return fenetre
+# Paramètres de la grille
+ROWS = 6
+COLS = 7
+NB_JETONS_V = 4
+CELL_SIZE = 100
+RADIUS = CELL_SIZE // 2 - 5
+
+# Couleurs des jetons
+PLAYER_COLORS = {0:"white", 1: "#FF0000", 2: "#FFD700"}
+
+# Grille de jeu (0 = vide, 1 = joueur 1, 2 = joueur 2)
+grid = [[0] * COLS for _ in range(ROWS)]
+liste_jetons = []
+current_player = 1  # j1 commence
+canvas = None
+canvas_tour = None
+winner = None
