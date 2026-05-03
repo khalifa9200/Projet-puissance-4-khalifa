1bb5e7d7b84854b005de4df1991474b503c60c2e
khalifa9200 <k.gherghout95@gmail.com>
2026-05-03 15:06:26 +0200

    khalifa

--- fenetre.py
+++ fenetre.py
@@ -1,19 +1,36 @@
 import tkinter as tk
-import main
-
-def ouvrir_fenetre():
-    main.fen_accueil()
+from main import fen_parties
+from set_et_match import fen_parties_set_et_match
+from robot import fenetre_robot
 
 fen = tk.Tk()
 fen.title("Puissance 4")
 
+
+button_style = {
+    "font": ("Arial", 14),
+    "width": 20,
+    "height": 2,
+    "bd": 5,
+    "relief": "raised",
+    "activebackground": "#32CD32",  
+    "bg": "#FFD700",  
+    "fg": "black"
+}
+
 label_haut  = tk.Label(fen, text="Mode de jeu")
-button_1vs1 = tk.Button(fen, text="1 VS 1", command=ouvrir_fenetre)
-button_1vsRobot = tk.Button(fen, text="1 VS ROBOT", command=ouvrir_fenetre)
+button_1vs1 = tk.Button(fen, text="1 VS 1", **button_style, command= fen_parties)
+button_1vsRobot = tk.Button(fen, text="1 VS ROBOT", **button_style, command=fenetre_robot)
+button_Set_et_Match = tk.Button(fen, text="Set et Match", **button_style, command=lambda: fen_parties_set_et_match(fen))
 
 label_haut.pack(padx=10, pady=10)
 button_1vs1.pack(padx=10, pady=10)
 button_1vsRobot.pack(padx=10, pady=10)
-
+button_Set_et_Match.pack(padx=10, pady=10)
 
 fen.mainloop()
+
+
+# Sources
+#https://www.unicode.org/emoji/charts/full-emoji-list.html
+#https://htmlcolorcodes.com/fr/
